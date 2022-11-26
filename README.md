eb7ffaf31e994eb739481345b644835fdb669dd3
rosallx <59135007+rosallx@users.noreply.github.com>
2022-11-26 23:03:30 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# quasarframework-qwitter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/quasarframework-azmapv)
---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,69 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+
+    <q-header bordered class="bg-white text-black">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title class="text-weight-bold">
+          
+          Qwitter
+        </q-toolbar-title>
+
+        
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer show-if-above v-model="leftDrawerOpen" side="left" bordered>
+      <q-icon 
+      class="q-pa-sm"
+      name="font_download"
+      size="lg"
+      color="primary" />
+
+          <q-list >
+      <q-item clickable v-ripple>
+        <q-item-section avatar>
+          <q-icon color="primary" name="bluetooth" size="md" />
+        </q-item-section>
+
+        <q-item-section class="text-h6 text-weight-bold">Home</q-item-section>
+      </q-item>
+
+     
+    </q-list>
+    </q-drawer>
+
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup () {
+    const leftDrawerOpen = ref(false)
+    const rightDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer () {
+        rightDrawerOpen.value = !rightDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
